35b57858d7bb29f862bb458215777f40d0dde11e
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-09-26 22:28:32 +0530

    updated website name in sidebar

--- symbiosis_react/src/components/Topbar/index.tsx
+++ symbiosis_react/src/components/Topbar/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleCollapsedSideNav } from "../../appRedux/actions";
 import { NAV_STYLE_DRAWER, NAV_STYLE_FIXED, NAV_STYLE_MINI_SIDEBAR, TAB_SIZE } from "../../constants/ThemeSetting";
-import { Layout, Input, Space } from "antd"; // Import Input and Space from Ant Design
+import { Layout, Input, Space, Typography } from "antd"; // Import Typography from Ant Design
 import UserProfile from "./UserProfile";
 import type { RootState } from "../../appRedux/store/index";
 
@@ -16,8 +16,12 @@ const Topbar = () => {
 
   return (
     <Header>
+         
       <div className="gx-flex-row gx-align-items-center" style={{ width: "100%" }}>
+            {/* Collapse Sidebar Button */}
+            
         <div className="gx-linebar gx-mr-3">
+               
           {navStyle === NAV_STYLE_DRAWER || ((navStyle === NAV_STYLE_FIXED || navStyle === NAV_STYLE_MINI_SIDEBAR) && width < TAB_SIZE) ? (
             <i
               className="gx-icon-btn icon icon-menu"
@@ -26,11 +30,19 @@ const Topbar = () => {
               }}
             />
           ) : null}
+              
         </div>
-
-        {/* Search Bar */}
+            {/* Website Name */}
+            
+        <Typography.Title level={3} style={{ color: "#fff", margin: 0 }}>
+               Your Website Name     
+        </Typography.Title>
+            {/* Search Bar */}
+            
         <div style={{ flex: 1 }}>
+               
           <Space>
+                  
             <Search
               placeholder="Search..."
               onSearch={(value) => {
@@ -38,17 +50,26 @@ const Topbar = () => {
                 console.log("Search value:", value);
               }}
               style={{ width: 200 }}
+              className="wieldy-search-bar"
             />
+                 
           </Space>
+              
         </div>
-
-        {/* User Profile */}
+            {/* User Profile */}
+            
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
+               
           <div className="gx-d-none gx-d-lg-block" style={{ paddingTop: "14px" }}>
+                  
             <UserProfile />
+                 
           </div>
+              
         </div>
+           
       </div>
+        
     </Header>
   );
 };
