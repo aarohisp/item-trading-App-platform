ea7ef2e0abd99e99baf0cc082744c826c5483dc1
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-09-27 19:24:33 +0530

    adjust position of search bar and user profile button

--- symbiosis_react/src/components/Topbar/index.tsx
+++ symbiosis_react/src/components/Topbar/index.tsx
@@ -1,27 +1,23 @@
 import React from "react";
+import { Layout, Input, Space, Typography } from "antd";
+import UserProfile from "./UserProfile";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleCollapsedSideNav } from "../../appRedux/actions";
 import { NAV_STYLE_DRAWER, NAV_STYLE_FIXED, NAV_STYLE_MINI_SIDEBAR, TAB_SIZE } from "../../constants/ThemeSetting";
-import { Layout, Input, Space, Typography } from "antd"; // Import Typography from Ant Design
-import UserProfile from "./UserProfile";
-import type { RootState } from "../../appRedux/store/index";
+import type { RootState } from "../../appRedux/store";
 
 const { Header } = Layout;
-const { Search } = Input; // Destructure Search component from Ant Design
+const { Search } = Input;
 
-const Topbar = () => {
+const Topbar: React.FC = () => {
   const dispatch = useDispatch();
   const { navStyle } = useSelector(({ settings }: RootState) => settings);
   const { navCollapsed, width } = useSelector(({ common }: RootState) => common);
 
   return (
     <Header>
-         
       <div className="gx-flex-row gx-align-items-center" style={{ width: "100%" }}>
-            {/* Collapse Sidebar Button */}
-            
         <div className="gx-linebar gx-mr-3">
-               
           {navStyle === NAV_STYLE_DRAWER || ((navStyle === NAV_STYLE_FIXED || navStyle === NAV_STYLE_MINI_SIDEBAR) && width < TAB_SIZE) ? (
             <i
               className="gx-icon-btn icon icon-menu"
@@ -30,19 +26,12 @@ const Topbar = () => {
               }}
             />
           ) : null}
-              
         </div>
-            {/* Website Name */}
-            
         <Typography.Title level={3} style={{ color: "#fff", margin: 0 }}>
-               Your Website Name     
+          Your Website Name
         </Typography.Title>
-            {/* Search Bar */}
-            
         <div style={{ flex: 1 }}>
-               
           <Space>
-                  
             <Search
               placeholder="Search..."
               onSearch={(value) => {
@@ -52,24 +41,19 @@ const Topbar = () => {
               style={{ width: 200 }}
               className="wieldy-search-bar"
             />
-                 
           </Space>
-              
         </div>
-            {/* User Profile */}
-            
+        <div className="dt-nav-wrapper">
+          <ul className="dt-nav d-lg-none">{/* Replace with your dropdown menu code */}</ul>
+          <ul className="dt-nav">{/* Replace with your dropdown menu code */}</ul>
+          <ul className="dt-nav">{/* Replace with your dropdown menu code */}</ul>
+        </div>
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem" }}>
-               
           <div className="gx-d-none gx-d-lg-block" style={{ paddingTop: "14px" }}>
-                  
             <UserProfile />
-                 
           </div>
-              
         </div>
-           
       </div>
-        
     </Header>
   );
 };
