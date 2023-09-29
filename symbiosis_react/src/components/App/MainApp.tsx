70e522f3bdf1622ae90538c4cf8dd1141ed2262d
aarohisp <112809842+aarohisp@users.noreply.github.com>
2023-09-29 14:54:59 +0530

    added prod description features

--- symbiosis_react/src/components/App/MainApp.tsx
+++ symbiosis_react/src/components/App/MainApp.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { Layout } from "antd";
+import { Layout, Image, Button } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import moment from "moment-timezone";
+import { ContactsOutlined } from "@ant-design/icons";
 import App from "../../routes/index";
 import Topbar from "../Topbar";
 import AppSidebar from "./AppSidebar";
@@ -27,6 +28,39 @@ const getContainerClass = (navStyle: string) => {
       return "gx-container-wrap";
     default:
       return "";
+  }
+};
+
+const styles: any = {
+  imageCarouselContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    position: "relative"
+  },
+  imageDisplay: {
+    display: "flex",
+    alignItems: "center",
+    position: "relative"
+  },
+  imageNavigator: {
+    position: "absolute",
+    right: "-30px",
+    top: "50%",
+    transform: "translateY(-50%)",
+    fontSize: "24px",
+    cursor: "pointer",
+    background: "transparent",
+    border: "none",
+    outline: "none"
+  },
+  productDetails: {
+    marginTop: "20px"
+  },
+  contactBtn: {
+    width: "150px",
+    marginTop: "10px"
   }
 };
 
@@ -79,11 +113,68 @@ const MainApp = () => {
     return () => clearTimeout(timer);
   }, []);
 
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      dispatch(updateWindowWidth(window.innerWidth));
-    });
-  }, [dispatch]);
+  function ImageCarousel() {
+    const IMAGE_SIZE = 300;
+    const AUTOPLAY_INTERVAL = 7000;
+    const IMAGE_SPACING = 20;
+    const images = [
+      "https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg",
+      "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+      "https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg",
+      "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+      "https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
+
+      // ... add more images as needed
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleArrowClick = () => {
+      setCurrentIndex((prevIndex) => {
+        if (prevIndex + 2 >= images.length) return 0;
+        return prevIndex + 1;
+      });
+    };
+
+    useEffect(() => {
+      window.addEventListener("resize", () => {
+        dispatch(updateWindowWidth(window.innerWidth));
+      });
+      const autoplay = setInterval(() => {
+        handleArrowClick();
+      }, AUTOPLAY_INTERVAL);
+
+      return () => clearInterval(autoplay); // Cleanup the interval when the component is unmounted
+    }, [dispatch]);
+
+    useEffect(() => {
+      window.addEventListener("resize", () => {
+        dispatch(updateWindowWidth(window.innerWidth));
+      });
+    }, [dispatch]);
+
+    return (
+      <div style={styles.imageCarouselContainer}>
+        <h1>Product Description</h1>
+        <Image.PreviewGroup>
+          <div style={styles.imageDisplay}>
+            <Image width={IMAGE_SIZE} src={images[currentIndex]} style={{ marginRight: `${IMAGE_SPACING}px` }} />
+            {images[currentIndex + 1] && <Image width={IMAGE_SIZE} src={images[currentIndex + 1]} />}
+            <button onClick={handleArrowClick} style={styles.imageNavigator}>
+              ➔
+            </button>
+          </div>
+        </Image.PreviewGroup>
+        <section style={styles.productDetails}>
+          <h2>Product Details</h2>
+          <p>This section provides details about the displayed product. You can expand upon this section to give more context or describe the product in detail.</p>
+          <Button type="primary" shape="round" icon={<ContactsOutlined />} style={styles.contactBtn}>
+            Contact Donor
+          </Button>
+        </section>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -92,13 +183,12 @@ const MainApp = () => {
         <Layout>
           {getNavStyles(navStyle)}
           <Content className={`gx-layout-content ${getContainerClass(navStyle)} `}>
-            <Dashboard />
             <App />
+            <ImageCarousel />
             <Footer>
               <div className="gx-layout-footer-content">
-                {/* get year from current year(YYYY) - next year (YY) */}
                 <div>
-                  © SevaSahayog (company). {new Date().getFullYear()}-{(new Date().getFullYear() + 1).toString().slice(2)}
+                  © Seva Sahyog Foundation Ltd (company). {new Date().getFullYear()}-{(new Date().getFullYear() + 1).toString().slice(2)}
                 </div>
               </div>
             </Footer>
@@ -108,4 +198,5 @@ const MainApp = () => {
     </>
   );
 };
+
 export default MainApp;
